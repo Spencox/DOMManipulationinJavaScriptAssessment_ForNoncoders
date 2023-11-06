8c007ae42093d4d09d16981537d4f3a08a6cd9ac
Spencer Cox <spencox@gmail.com>
2023-11-06 16:36:28 -0600

    initia files saved to project

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// This function should retrieve all the project objects from projects array.
+// It should then traverse over the array to create individual cards displaying each project details.
+function loadProjects() {
+  // Write your code here
+
+}
+
+// This function should create a new project by retrieving details from the form.
+// The project is then added to the projects array and displayed.
+function saveNewProject() {
+
+  // Write your code here
+  // Get the new project details by using the DOM elements
+  
+  
+  // Create a new project object
+  
+
+  // Add the new project object to the projects array 
+  
+
+  // Load the projects after adding the new project
+  loadProjects();
+
+  // Clear the values of the New Project Details Form after adding the new project
+  document.getElementById("title").value = "";
+  document.getElementById("desc").value = "";
+  document.getElementById("image").value = "";
+}
